c3c8583dcdd35dda53d8e373e39da20def7f39dc
Oscar Hoodsmith <55330213+JellyNutJim@users.noreply.github.com>
2025-03-20 08:57:04 +0000

    new data type

---
+++ src/types/geometry.rs
@@ -0,0 +1,5 @@
+pub enum Geometry {
+    Voxel(),
+    FourHeightSurface(),
+    FourPointSurface()
+}
--- src/types/mod.rs
+++ src/types/mod.rs
@@ -1,5 +1,7 @@
 mod vec3;
 mod voxel;
+mod geometry;
 
 pub use vec3::Vec3;
 pub use voxel::Voxel;
+pub use geometry::Geometry;
